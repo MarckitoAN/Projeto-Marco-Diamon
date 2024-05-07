2c6cd7604f9778c84700274995e1ca3a9d392f7d
Joao Vitor Diamon Roldan dos Santos <143654414+JoaoVitorDiamon@users.noreply.github.com>
2024-05-07 17:14:38 -0300

    Update: Correção de Nomenclatura

--- BancoDeDados/gerenciadorDeLoja.sql
+++ BancoDeDados/gerenciadorDeLoja.sql
@@ -4,33 +4,33 @@ use GerenciamentoDeLojasADM;
 
 CREATE TABLE Cliente
 (
-    id_cliente    INT AUTO_INCREMENT,
-    nome_cliente  VARCHAR(100) NOT NULL,
-    rg_cliente    VARCHAR(20),
-    cpf_cliente   VARCHAR(20),
+    id    INT AUTO_INCREMENT,
+    nome  VARCHAR(100) NOT NULL,
+    rg    VARCHAR(20),
+    cpf   VARCHAR(20),
     telefone VARCHAR(11),
     rua           VARCHAR(100),
     bairro        VARCHAR(100),
     cidade        VARCHAR(100),
     estado        VARCHAR(100),
-    email_cliente VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     senha_hash    VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id_cliente),
-    CONSTRAINT unique_email UNIQUE (email_cliente)
+    PRIMARY KEY (id),
+    CONSTRAINT unique_email UNIQUE (email)
 );
 
 create table Produto
 (
-    id_produto     integer auto_increment,
-    nome_produto   varchar(45)    not null,
-    descri_produto varchar(255),
-    marca_produto  varchar(45)    not null,
-    preco_prod     decimal(10, 2) not null,
-    tipo_prod      varchar(45)    not null,
+    id     integer auto_increment,
+    nome   varchar(45)    not null,
+    descri varchar(255),
+    marca  varchar(45)    not null,
+    preco     decimal(10, 2) not null,
+    tipo      varchar(45)    not null,
     tamanho        varchar(5)     not null,
-    cor_prod       varchar(25)    not null,
+    cor       varchar(25)    not null,
     estoque        int            not null,
-    primary key (id_produto)
+    primary key (id)
 );
 
 
@@ -41,8 +41,8 @@ CREATE TABLE Pedido_Produto
     id_produto        INTEGER,
     Quantidade Int,
     PRIMARY KEY (id_Pedido_Produto),
-    CONSTRAINT fk_idproduto FOREIGN KEY (id_produto) REFERENCES Produto (id_produto),
-    constraint fk_pedido_id foreign key (id_pedido) references Pedido (id_pedido)
+    CONSTRAINT id_produto_fk FOREIGN KEY (id_produto) REFERENCES Produto (id),
+    constraint id_pedido_fk foreign key (id_pedido) references Pedido (id)
 
 );
 
@@ -50,29 +50,29 @@ CREATE TABLE Pedido_Produto
 
 CREATE TABLE Estoque
 (
-    id_estoque   INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_produto   INTEGER,
+    id   INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    id   INTEGER,
     Quantidade   INTEGER UNSIGNED,
     Data_Entrada DATE,
     Data_Saida   DATE,
     Motivo_Saida VARCHAR(45),
     PRIMARY KEY (id_Estoque),
-    constraint fk_pk_estoque foreign key (id_produto) references Produto (id_produto)
+    constraint id_produto_fk foreign key (id_produto) references Produto (id_produto)
 );
 
 
 CREATE TABLE Pedido
 (
-    id_pedido       INTEGER AUTO_INCREMENT,
-    data_pedido     DATE,
-    id_cliente      integer,
-    id_produto integer,
+    id       INTEGER AUTO_INCREMENT,
+    data     DATE,
+    id      integer,
+    id integer,
     forma_pagamento varchar(200),
     parcelas        int,
     valor_total     DECIMAL(10, 2),
     PRIMARY KEY (id_pedido),
-    CONSTRAINT fk_id_clientePedido FOREIGN KEY (id_cliente) REFERENCES Cliente (id_cliente),
-	CONSTRAINT fk_id_ProdutoPedido FOREIGN KEY (id_produto) REFERENCES Produto (id_produto)
+    CONSTRAINT id_cliente_fk FOREIGN KEY (id_cliente) REFERENCES Cliente (id_cliente),
+	CONSTRAINT id_produto_fk FOREIGN KEY (id_produto) REFERENCES Produto (id_produto)
 
 );
 
