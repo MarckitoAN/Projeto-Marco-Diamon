900c740857b8195bacf2d08c916711dfd38591d6
Joao Vitor Diamon Roldan dos Santos <143654414+JoaoVitorDiamon@users.noreply.github.com>
2024-05-07 22:17:03 -0300

    FEAT: ATIVIDADE FEITA EM SALA

---
+++ EntregueiNaHora/CriptografiaHash.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System;
+
+public class Hash
+{
+    private HashAlgorithm _algoritmo;
+
+    public Hash(HashAlgorithm algoritmo)
+    {
+        _algoritmo = algoritmo;
+    }
+
+    public string CriptografarSenha(string senha)
+    {
+        var encodedValue = Encoding.UTF8.GetBytes(senha);
+        var encryptedPassword = _algoritmo.ComputeHash(encodedValue);
+
+        var sb = new StringBuilder();
+        foreach (var caracter in encryptedPassword)
+        {
+            sb.Append(caracter.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    public bool VerificarSenha(string senhaDigitada, string senhaCadastrada)
+    {
+        if (string.IsNullOrEmpty(senhaCadastrada))
+            throw new NullReferenceException("Cadastre uma senha.");
+
+        var encryptedPassword = _algoritmo.ComputeHash(Encoding.UTF8.GetBytes(senhaDigitada));
+
+        var sb = new StringBuilder();
+        foreach (var caractere in encryptedPassword)
+        {
+            sb.Append(caractere.ToString("X2"));
+        }
+
+        return sb.ToString() == senhaCadastrada;
+    }
+}
---
+++ EntregueiNaHora/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Usuario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            MySqlCommand cmd;
+            string connection = "server=localhost;port=3306;Database=GerenciamentoDeLojasADM;uid=root;";
+            MySqlConnection conn = new MySqlConnection(connection);
+
+
+            try
+            {
+                conn.Open();
+                {
+                    using(Users users = new Users())
+                    { 
+                    users.CadatroUsuario();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        Console.WriteLine("Conexao Aberta com Sucesso:");
+                    }
+
+                    command = "insert into User (nome_loja,contato,email,cnpj,senha_hash) values (@nome_loja,@contato,@email,@cnpj,@senha_hash)";
+                    cmd = new MySqlCommand(command, conn);
+
+                    cmd.Parameters.AddWithValue("@nome_loja", users.nomeDaLoja);
+                    cmd.Parameters.AddWithValue("@contato", users.contato);
+                    cmd.Parameters.AddWithValue("@email", users.email);
+                    cmd.Parameters.AddWithValue("@cnpj", users.cnpj);
+                    cmd.Parameters.AddWithValue("@senha_hash", users.Senha);
+                    int linhasafetadas = cmd.ExecuteNonQuery();
+
+                    if (linhasafetadas == 0)
+                    {
+                        throw new Exception("Nenhuma linha foi afetada pela consulta.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Linhas afetadas:{0}", linhasafetadas);
+                    }
+                }
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
