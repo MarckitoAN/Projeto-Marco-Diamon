49f4b9d84ebb29346e776bc9cce452110e035ea5
Joao Vitor <joao.santos2112@etec.sp.gov.br>
2024-05-08 21:17:16 -0300

    UPDATE: Classe DAO

---
+++ ConexaoComBancoDeDados/Usuario/Dao.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario
+{
+    public class Dao
+    {
+        public MySqlConnection conexaoBancoDeDados;
+        private string stringConexao = "server=localhost;port=3306;Database=GerenciamentoDeLojasADM;uid=root;";
+        public MySqlCommand comandoSql;
+        private string comandoSqlString;
+
+        public void ConectarBancoDeDados()
+        {
+            try
+            {
+                conexaoBancoDeDados = new MySqlConnection(stringConexao);
+                conexaoBancoDeDados.Open();
+                Console.WriteLine("Conexão estabelecida com sucesso!");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+         
+            }
+        }
+
+        public void DefinirComandoSql(string cmd) {
+
+            comandoSql = new MySqlCommand(comandoSqlString, conexaoBancoDeDados);
+        
+        }
+
+
+        public void VerificarLinhasAfetadas()
+        {
+            int linhasafetadas = comandoSql.ExecuteNonQuery();
+
+            if (linhasafetadas == 0)
+            {
+                throw new Exception("Nenhuma linha foi afetada pela consulta.");
+            }
+            else
+            {
+                Console.WriteLine("Linhas afetadas:{0}", linhasafetadas);
+            }
+        }
+
+
+        public void FecharConexao()
+        {
+            conexaoBancoDeDados.Close();
+        }
+    }
+}
---
+++ ConexaoComBancoDeDados/Usuario/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Usuario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dao dao = new Dao();
+
+            try
+            {
+                dao.ConectarBancoDeDados();
+                {
+                    using (Users users = new Users())
+                    {
+                        users.CadatroUsuario();
+
+                        dao.DefinirComandoSql("insert into User (nome_loja,contato,email,cnpj,senha_hash) values (@nome_loja,@contato,@email,@cnpj,@senha_hash)");
+                        dao.comandoSql.Parameters.AddWithValue("@nome_loja", users.nomeDaLoja);
+                        dao.comandoSql.Parameters.AddWithValue("@contato", users.contato);
+                        dao.comandoSql.Parameters.AddWithValue("@email", users.email);
+                        dao.comandoSql.Parameters.AddWithValue("@cnpj", users.cnpj);
+                        dao.comandoSql.Parameters.AddWithValue("@senha_hash", users.Senha);
+                        dao.VerificarLinhasAfetadas();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            dao.FecharConexao();
+            Console.ReadKey();
+        }
+    }
+}
