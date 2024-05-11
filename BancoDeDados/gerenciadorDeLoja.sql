346cc89b6ec97131560c7ca31aa559e435d16f84
Joao Vitor <joao.santos2112@etec.sp.gov.br>
2024-05-10 21:12:24 -0300

    UPDATE: Arquivo SQL:atualizando as nomenclatura

--- BancoDeDados/gerenciadorDeLoja.sql
+++ BancoDeDados/gerenciadorDeLoja.sql
@@ -2,90 +2,87 @@ create database GerenciamentoDeLojasADM;
 
 use GerenciamentoDeLojasADM;
 
+
 CREATE TABLE Cliente
 (
-    id    INT AUTO_INCREMENT,
-    nome  VARCHAR(100) NOT NULL,
-    rg    VARCHAR(20),
-    cpf   VARCHAR(20),
-    telefone VARCHAR(11),
-    rua           VARCHAR(100),
-    bairro        VARCHAR(100),
-    cidade        VARCHAR(100),
-    estado        VARCHAR(100),
-    email VARCHAR(100) NOT NULL,
-    senha_hash    VARCHAR(100) NOT NULL,
+    id       INT AUTO_INCREMENT,
+    nome     VARCHAR(100) NOT NULL,
+    rg       VARCHAR(20),
+    cpf      VARCHAR(20),
+    telefone VARCHAR(20),
+    rua      VARCHAR(100),
+    bairro   VARCHAR(100),
+    cidade   VARCHAR(100),
+    estado   VARCHAR(100),
+    email    VARCHAR(100) NOT NULL,
+    senha    VARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT unique_email UNIQUE (email)
 );
 
 create table Produto
 (
-    id     integer auto_increment,
-    nome   varchar(45)    not null,
-    descri varchar(255),
-    marca  varchar(45)    not null,
+    id        integer auto_increment,
+    nome      varchar(45)    not null,
+    descricao varchar(255),
+    marca     varchar(45)    not null,
     preco     decimal(10, 2) not null,
     tipo      varchar(45)    not null,
-    tamanho        varchar(5)     not null,
+    tamanho   varchar(5)     not null,
     cor       varchar(25)    not null,
-    estoque        int            not null,
     primary key (id)
 );
 
+show tables;
 
 CREATE TABLE Pedido_Produto
 (
-    id_Pedido_Produto INTEGER UNSIGNED AUTO_INCREMENT,
+    id_pedido_produto INTEGER UNSIGNED AUTO_INCREMENT,
     id_pedido         INTEGER,
     id_produto        INTEGER,
-    Quantidade Int,
-    PRIMARY KEY (id_Pedido_Produto),
+    quantidade        int,
+    PRIMARY KEY (id_pedido_produto),
     CONSTRAINT id_produto_fk FOREIGN KEY (id_produto) REFERENCES Produto (id),
     constraint id_pedido_fk foreign key (id_pedido) references Pedido (id)
-
 );
 
 
 
 CREATE TABLE Estoque
 (
-    id   INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    id   INTEGER,
+    id           INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    id_produto   INTEGER,
     Quantidade   INTEGER UNSIGNED,
     Data_Entrada DATE,
     Data_Saida   DATE,
     Motivo_Saida VARCHAR(45),
-    PRIMARY KEY (id_Estoque),
-    constraint id_produto_fk foreign key (id_produto) references Produto (id_produto)
+    PRIMARY KEY (id),
+    constraint fk_pk_estoque foreign key (id_produto) references Produto (id)
 );
 
 
 CREATE TABLE Pedido
 (
-    id       INTEGER AUTO_INCREMENT,
-    data     DATE,
-    id      integer,
-    id integer,
+    id              INTEGER AUTO_INCREMENT,
+    data            DATE,
+    id_cliente      integer,
     forma_pagamento varchar(200),
     parcelas        int,
     valor_total     DECIMAL(10, 2),
-    PRIMARY KEY (id_pedido),
-    CONSTRAINT id_cliente_fk FOREIGN KEY (id_cliente) REFERENCES Cliente (id_cliente),
-	CONSTRAINT id_produto_fk FOREIGN KEY (id_produto) REFERENCES Produto (id_produto)
-
+    PRIMARY KEY (id),
+    CONSTRAINT fk_id_clientePedido FOREIGN KEY (id_cliente) REFERENCES Cliente (id),
+    CONSTRAINT valor_check check (valor_total >= 0)
 );
 
-create table Users
+create table User
 (
-    id_users integer auto_increment,
+    id         integer auto_increment,
     nome_loja  varchar(255) not null,
-    contato    varchar(16),
-    email      varchar(255),
-    cnpj       varchar(14),
-    senha_hash varchar(255),
-    primary key (id_loja),
+    contato    varchar(16)  not null,
+    email      varchar(255) not null,
+    cnpj       varchar(14)  not null,
+    senha_hash varchar(255) not null,
+    primary key (id),
     CONSTRAINT un_cnpj unique (cnpj),
     CONSTRAINT un_email unique (email)
 );
- 
